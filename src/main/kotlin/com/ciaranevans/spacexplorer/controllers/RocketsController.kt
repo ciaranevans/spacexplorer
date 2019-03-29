5c4071091954cb46922191d9b9ecbf219219ab23
Ciaran Evans <ciaran.j.evans@sky.com>
2019-03-29 11:00:34 +0000

    Add Cross Origin

--- src/main/kotlin/com/ciaranevans/spacexplorer/controllers/RocketsController.kt
+++ src/main/kotlin/com/ciaranevans/spacexplorer/controllers/RocketsController.kt
@@ -3,11 +3,13 @@ package com.ciaranevans.spacexplorer.controllers
 import com.ciaranevans.spacexplorer.Rocket
 import com.ciaranevans.spacexplorer.services.RocketsService
 import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin
 @RestController
 @RequestMapping("/rockets")
 class RocketsController(
